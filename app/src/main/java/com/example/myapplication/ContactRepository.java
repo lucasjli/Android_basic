14cc74f753973fddae61b93bda11266907dff905
lucas <lijie.lucas@hotmail.com>
2025-05-12 23:24:40 +1200

    Add ContactRepository class.

---
+++ app/src/main/java/com/example/myapplication/ContactRepository.java
@@ -0,0 +1,49 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import androidx.lifecycle.LiveData;
+import java.util.List;
+
+public class ContactRepository {
+    private ContactDao contactDao;
+    private LiveData<List<Contact>> allContacts;
+    
+    ContactRepository(Context context) {
+        ContactRoomDatabase db = ContactRoomDatabase.getDatabase(context);
+        contactDao = db.contactDao();
+        allContacts = contactDao.getAllContacts();
+    }
+    
+    public void insert(Contact contact) {
+        new InsertAsyncTask().execute(contact);
+    }
+    
+    private class InsertAsyncTask extends AsyncTask<Contact, Void, Void> {
+        @Override
+        protected Void doInBackground(final Contact... params) {
+            for (Contact contact : params) {
+                contactDao.insert(contact);
+            }
+            return null;
+        }
+    }
+    
+    public void update(Contact contact) {
+        new UpdateAsyncTask().execute(contact);
+    }
+    
+    private class UpdateAsyncTask extends AsyncTask<Contact, Void, Void> {
+        @Override
+        protected Void doInBackground(final Contact... params) {
+            for (Contact contact : params) {
+                contactDao.update(contact);
+            }
+            return null;
+        }
+    }
+    
+    public LiveData<List<Contact>> getAllContacts() {
+        return allContacts;
+    }
+}
